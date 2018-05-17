cac9f00277710ea55ea5fec8fe74ecd80cfc421a
Anahita <anahita.vahdani@neuf.fr>
2018-05-17 19:09:32 +0200

    create empire slogan

--- etoileamour/src/App.js
+++ etoileamour/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Characters from './components/Characters'
 import Logo from './components/Carousel'
+import Slogan from './components/Slogan';
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Logo />
+        <Slogan />
         <Characters />
       </div>
     );
---
+++ etoileamour/src/components/Slogan.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Slogan = () => <p>Vous subirez l'amour, pour notre maître !</p>
+
+export default Slogan
