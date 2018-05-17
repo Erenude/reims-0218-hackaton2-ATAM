ef3505bf9d97d5b59f5b14f9e57d6103296a5928
GalloisMax <max.gallois@outlook.fr>
2018-05-17 17:41:17 +0200

    ajout carousel composant Logo

--- etoileamour/src/App.js
+++ etoileamour/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Characters from './components/Characters'
+import Logo from './components/Carousel'
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Logo />
         <Characters />
       </div>
     );
