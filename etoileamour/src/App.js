7410510901a25adc710216b2bc891a634f42c6df
Arno Wilder Reims <arnocrea@gmail.com>
2018-05-17 14:00:59 +0200

    fetch characters from api

--- etoileamour/src/App.js
+++ etoileamour/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Test from './components/Test'
+import Characters from './components/Characters'
 
 class App extends Component {
   render() {
@@ -14,7 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Test />
+        <Characters />
       </div>
     );
   }
---
+++ etoileamour/src/components/Characters.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class Characters extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            loading: true,
+            characters: {
+                name: ''
+            }
+        }
+    }
+    render() {
+        return <div>
+            { this.state.loading && <p>Loading</p> }
+            <p>Character: {this.state.characters.name}</p>
+            </div>
+    }
+
+    componentDidMount() {
+        console.log('characters did mount')
+        const url = 'https://cdn.rawgit.com/akabab/starwars-api/0.2.1/api/all.json'
+        fetch(url)
+        .then(res => res.json())
+        .then(characters => console.log(characters))
+    }
+}
+
+export default Characters
